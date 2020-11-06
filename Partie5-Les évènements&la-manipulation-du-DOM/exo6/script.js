5d43226b44f95e80f83f70903a1de1345b9d8a62
CristinaOanaManole <oana_manole@yahoo.com>
2020-11-06 11:24:51 +0100

    correcions

---
+++ Partie5-Les évènements&la-manipulation-du-DOM/exo6/script.js
@@ -0,0 +1,8 @@
+let form = document.getElementById('form');
+
+form.onfocus = function ()
+
+    form.onmouseover.style.borderColor = 'black, 1rem';
+
+
+    form.onmouseout.style.borderColor = 'none';
