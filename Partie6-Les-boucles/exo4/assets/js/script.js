514b995b9852aa0f8aa54faa19ae35cac8465f9a
CristinaOanaManole <oana_manole@yahoo.com>
2020-11-10 15:08:08 +0100

    correction ok

--- Partie6-Les-boucles/exo4/assets/js/script.js
+++ Partie6-Les-boucles/exo4/assets/js/script.js
@@ -1,1 +1,15 @@
-let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi','Dimanche'];
+let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+let weekDay = document.getElementById('weekDay');
+
+days.forEach(function (element) {
+    let tableTd = document.createElement('td');
+
+    if ((element == 'Samedi') || (element == 'Dimanche')) {
+        tableTd.innerHTML = element.bold();
+    } else {
+        tableTd.innerHTML = element;
+    }
+
+    weekDay.appendChild(tableTd);
+});
